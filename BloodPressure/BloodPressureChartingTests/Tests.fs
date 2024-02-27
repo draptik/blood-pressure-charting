91bfe913bdccf662eec1543274717a50d95a8f21
Patrick Drechsler <patrick@pdrechsler.de>
2024-02-27 02:29:22 +0100

    📝 docs: run test to create plot

--- BloodPressure/BloodPressureChartingTests/Tests.fs
+++ BloodPressure/BloodPressureChartingTests/Tests.fs
@@ -5,6 +5,9 @@ open BloodPressureCharting
 open Xunit
 open Data
 
+(*
+    We are abusing unit tests to run the program and generate the plot.
+*)
 [<Fact>]
 let ``Sample plot`` () =
     let input = Importer.importData "./SampleData/input_chatgpt.csv"
---
+++ BloodPressure/README.md
@@ -0,0 +1,5 @@
+# Charting
+
+We are abusing unit tests to run the program and generate the plot.
+
+Run the test. It will generate a plot and open it in your default browser.
