e3565d759f11aeb7eaf2ba9aa39a41d997393164
Patrick Drechsler <patrick@pdrechsler.de>
2024-02-28 21:18:15 +0100

    ✨ add simple starter script for console app

--- BloodPressure/run.sh
+++ BloodPressure/run.sh
@@ -5,6 +5,12 @@ SAMPLE_DATA_PATH="./BloodPressureChartingTests/SampleData"
 SAMPLE_DATA_FILE="input_chatgpt.csv"
 SAMPLE_DATA="${SAMPLE_DATA_PATH}/${SAMPLE_DATA_FILE}"
 
+if [ $# -eq 0 ]; then
+  INPUT_DATA="${SAMPLE_DATA}"
+else
+  INPUT_DATA="${1}"
+fi
+
 dotnet run \
   --project "${CONSOLE_APP_PATH}" \
-  --inputfile "${SAMPLE_DATA}"
+  --inputfile "${INPUT_DATA}"
